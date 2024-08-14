c7f71376577110b1b2386eeecdd60eeb686714aa
Conannia <conannia17@gmail.com>
2024-08-14 21:48:17 +0700

    get data

--- Fullstack.UI/src/app/components/karyawan/karyawan-list/karyawan-list.component.ts
+++ Fullstack.UI/src/app/components/karyawan/karyawan-list/karyawan-list.component.ts
@@ -1,41 +1,31 @@
 import { Component, OnInit } from '@angular/core';
-import { CommonModule } from '@angular/common'; // Import CommonModule
+import { CommonModule } from '@angular/common';
+import { HttpClient, HttpClientModule } from '@angular/common/http'; // Import HttpClient dan HttpClientModule
 import { Karyawan } from '../../../models/karyawan.model';
 
 @Component({
   selector: 'app-karyawan-list',
   standalone: true,
-  imports: [CommonModule],  // Tambahkan CommonModule di sini
+  imports: [CommonModule, HttpClientModule],  // Tambahkan HttpClientModule di sini
   templateUrl: './karyawan-list.component.html',
   styleUrls: ['./karyawan-list.component.css']
 })
 export class KaryawanListComponent implements OnInit {
 
-  karyawan: Karyawan[] = [
-    {
-      id: 1,
-      nama: 'Nurul',
-      email: 'c@sas.com',
-      phone: 121212,
-      gaji: 2000000,
-      divisi: 'finance',
-      level: 'Junior'
-    },
-    {
-      id: 2,
-      nama: 'Khusnia',
-      email: 'c@sas.com',
-      phone: 121212,
-      gaji: 15000000,
-      divisi: 'finance',
-      level: 'Senior'
-    }
-  ];
+  karyawan: Karyawan[] = [];
 
-  constructor() {}
+  constructor(private http: HttpClient) {}
 
   ngOnInit(): void {
-    // Logic tambahan jika diperlukan
+    this.http.get<Karyawan[]>('https://localhost:7273/api/controller')
+      .subscribe(
+        (data) => {
+          this.karyawan = data;  // Mengisi array karyawan dengan data dari API
+        },
+        (error) => {
+          console.error('Error fetching karyawan data:', error);
+        }
+      );
   }
 
 }
--- KaryawanApi/Karyawan.API/Karyawan.API/obj/Debug/net6.0/Karyawan.API.AssemblyInfo.cs
+++ KaryawanApi/Karyawan.API/Karyawan.API/obj/Debug/net6.0/Karyawan.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Karyawan.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8404356a4e1ce5fa342590456bf5c08c47f83a4e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Karyawan.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Karyawan.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
